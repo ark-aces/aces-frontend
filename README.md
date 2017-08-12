7da96b2e3c28cc7e894445511310a7e1481a70b5
Brady Olsen <brady@tangocard.com>
2017-08-12 15:14:47 -0700

    replace angular-starter with angular-cli

--- README.md
+++ README.md
@@ -5,13 +5,9 @@ contracts by sending a payment transaction using the [Ark Wallet](https://github
 
 ## Development Stack
 
-- [Angular 4 Webpack Starter](https://github.com/AngularClass/angular-starter)
 - [Angular 4 Framework](https://angular.io/)
 - [Bootstrap 3](http://getbootstrap.com/)
 - [Bootstrap 3 SB Admin 2 Theme](https://blackrockdigital.github.io/startbootstrap-sb-admin-2/)
-
-This application was initially created using [Angular4 Webpack Starter](https://github.com/AngularClass/angular-starter)
-and follows the tools and conventions defined in the starter.
 
 ## Running Application
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,10 @@
-/**
- * Angular 2 decorators and services
- */
-import {
-  Component,
-  OnInit,
-  ViewEncapsulation
-} from '@angular/core';
-import { AppState } from './app.service';
+import { Component } from '@angular/core';
 
-/**
- * App Component
- * Top Level Component
- */
 @Component({
-  selector: 'app',
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: [
-    './app.component.css'
-  ],
-  templateUrl: './app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public angularclassLogo = 'assets/img/angularclass-avatar.png';
-  public name = 'Angular 2 Webpack Starter';
-  public url = 'https://twitter.com/AngularClass';
-
-  constructor(
-    public appState: AppState
-  ) {}
-
-  public ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-  }
-
+export class AppComponent {
+  title = 'app';
 }
