fcdf733c459075292bb42e13cf2f332364bbbea1
Brady Olsen <brady@tangocard.com>
2017-08-13 22:48:17 -0700

    use form multipart post

--- src/app/eth-contract-deploy/aces-service/http-eth-contract-deploy.service.ts
+++ src/app/eth-contract-deploy/aces-service/http-eth-contract-deploy.service.ts
@@ -11,11 +11,18 @@ export class HttpEthContractDeployService extends EthContractDeployService {
   }
 
   create(createEthContractForm: CreateEthContractForm): Observable<EthContractResponse> {
-    return this.http.post('http://localhost:8080/contracts', createEthContractForm);
+    const input = new FormData();
+    input.append('returnArkAddress', createEthContractForm.returnArkAddress);
+    input.append('gasLimit', createEthContractForm.gasLimit.toString());
+    input.append('abiJson', new Blob([createEthContractForm.abiJson], {type: 'text/plain'}));
+    input.append('code', new Blob([createEthContractForm.code], {type: 'text/plain'}));
+    input.append('paramsJson', new Blob([createEthContractForm.paramsJson], {type: 'text/plain'}));
+
+    return this.http.post('https://aces-ark.io/aces-api/contracts', input);
   }
 
   get(token: string): Observable<EthContractResponse> {
-    return this.http.get<EthContractResponse>('http://localhost:8080/contracts/' + token);
+    return this.http.get<EthContractResponse>('https://aces-ark.io/aces-api/contracts/' + token);
   }
 
 }
