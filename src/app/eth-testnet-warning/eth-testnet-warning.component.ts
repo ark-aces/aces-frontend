66661b0a4758880211c6f935c7e4e17727f2e749
Brady Olsen <brady@tangocard.com>
2017-08-29 00:39:44 -0700

    add eth netnet info

--- src/app/eth-testnet-warning/eth-testnet-warning.component.ts
+++ src/app/eth-testnet-warning/eth-testnet-warning.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
     <div class="alert alert-warning" role="alert">
         <strong>Warning:</strong>
         This service is configured to use the
-            <a href="https://ropsten.etherscan.io">Ethereum Test Network (ropsten)</a>.
+            <a href="https://ropsten.etherscan.io">Ethereum Testnet (ropsten) 
+                <i class="fa fa-external-link"></i></a>.
+            The Ark-Eth rate is 1/100 the real rate for Testnet transactions.
     </div>
   `
 })
